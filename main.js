640a623fef174199de7e7ce50258020cb79e403d
Aman-kinger <amankinger01@gmail.com>
2024-01-20 15:24:06 +0530

    add function to remove book

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 
 const books = []
 
-function Book(title,author,pages,read){
+class Book {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        const info = `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
-        return info;
-    }
+    this.info = function () {
+      const info = `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+      return info;
+    };
+  }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Not Read');
+function addNewBook(book){
+    books.push(book)
+}
+
+function removeBook(bookTitle){
+  books.splice(books.findIndex((book) => book.title === bookTitle), 1)
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 books.push(theHobbit);
 
-const harryPotter = new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 223, 'Read');
+const harryPotter = new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 223, true);
 books.push(harryPotter)
 
-const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1137, 'Read');
+const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1137, true);
 books.push(theLordOfTheRings)
 
-const theHungerGames = new Book('The Hunger Games', 'Suzanne Collins', 374, 'Read');
+const theHungerGames = new Book('The Hunger Games', 'Suzanne Collins', 374, true);
 books.push(theHungerGames)
 
 
@@ -29,6 +39,7 @@ const booksGrid = document.getElementById('booksGrid')
 const addBookForm = document.getElementById('addBookForm')
 const addBookModal = document.getElementById('addBookModal')
 const overlay = document.getElementById('overlay')
+
 
 const createBookCard = (book) => {
     const bookCard = document.createElement('div')
@@ -58,7 +69,7 @@ const createBookCard = (book) => {
     pages.textContent = `${book.pages} pages`
     removeBtn.textContent = 'Remove'
   
-    if (book.read == 'Read') {
+    if (book.read) {
       readBtn.textContent = 'Read'
       readBtn.classList.add('btn-light-green')
     } else {
@@ -75,9 +86,9 @@ const createBookCard = (book) => {
     booksGrid.appendChild(bookCard)
   }
 
-for (let book of books) {
+  for (let book of books) {
     createBookCard(book)
-} 
+}
 
 const openAddBookModal = () => {
   addBookForm.reset()
@@ -92,6 +103,55 @@ const closeAddBookModal = () => {
   errorMsg.textContent = ''
 }
 
+const getBookFromInput = () => {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const read = document.getElementById('isRead').checked
+  return new Book(title, author, pages, read)
+}
+
+const addBook = (e) => {
+  e.preventDefault()
+  const newBook = getBookFromInput()
+  if (newBook.title === '' || newBook.author === '' || newBook.pages === '') {
+    errorMsg.textContent = 'Please fill out all fields'
+    errorMsg.classList.add('active')
+  } else if(newBook in books){
+    errorMsg.textContent = 'Book already exists'
+    errorMsg.classList.add('active')
+  }
+  else {
+    addNewBook(newBook)
+    createBookCard(newBook)
+    closeAddBookModal()
+  }
+}
+
+const updateBooksGrid = () => {
+  resetBooksGrid()
+  for (let book of books) {
+    createBookCard(book)
+  }
+}
+
+const resetBooksGrid = () => {
+  booksGrid.innerHTML = ''
+}
+
+const removeBookCard = (e) => {
+  const bookCard = e.target.parentNode.parentNode
+  const bookTitle = bookCard.firstChild.textContent.replace(/"/g, '')
+  removeBook(bookTitle)
+  updateBooksGrid()
+}
 
 addBookBtn.onclick = openAddBookModal
 overlay.onclick = closeAddBookModal
+addBookForm.onsubmit = addBook
+booksGrid.onclick = (e) => {
+  if (e.target.classList.contains('btn-remove')) {
+    removeBookCard(e)
+  }
+}
+
